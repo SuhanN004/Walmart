c606e5eebec5885b6e04f4e357b28e55d729bc10
Suhan004 <mewtwokabadla93@gmail.com>
2026-04-08 16:47:21 +0530

    updated backend again

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors({
-origin: process.env.CLIENT_URL,
-credentials: true
-}));
+// app.use(cors({
+// origin: process.env.CLIENT_URL,
+// credentials: true
+// }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
