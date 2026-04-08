ba1a11e60d0efaa1388adf4691061f76d512a977
Suhan004 <mewtwokabadla93@gmail.com>
2026-04-08 16:52:06 +0530

    updated backend again

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const paymentRoutes = require("./routes/payment");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+app.use(cors());
 // app.use(cors({
 // origin: process.env.CLIENT_URL,
 // credentials: true
